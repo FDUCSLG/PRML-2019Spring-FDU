6106aee4208a27c93983e05cec2c83b8de359e2b
ichn-hu <zfhu16@fudan.edu.cn>
2019-03-12 11:35:15 +0800

    add grading

--- assignment-1/16307130177/source.py
+++ assignment-1/16307130177/source.py
@@ -1,5 +1,6 @@
 import os
 os.sys.path.append('..')
+# use the above line of code to surpass the top module barrier
 from handout import get_data
 import numpy as np
 import matplotlib.pyplot as plt
