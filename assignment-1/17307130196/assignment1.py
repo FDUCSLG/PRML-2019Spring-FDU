9e3e86bc78d7164e33eb8af7b0ad8411fa10a0b6
Josep-h <2870952487@qq.com>
2019-03-15 19:58:03 +0800

    Begin of the project

---
+++ assignment-1/17307130196/assignment1.py
@@ -0,0 +1,10 @@
+#FINAL GOAL: Use Nonparametric Methods to get probability distribution
+
+#This is the first task of Assignment 1
+#In this file, I will try to figure out what will happen with different numbers of samples.
+
+#Histogram Method, Kernel Method and the Nearest Neighbor Method will be showed here.
+#-------------
+#First the Histogram Method
+#Simply partition x into 
+
