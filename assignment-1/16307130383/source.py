8b4d6d680384584742de92169d03e060c39ce2b8
yilin <yilinyan1030@gmail.com>
2019-03-19 00:27:54 +0800

    explore params & final plots

--- assignment-1/16307130383/source.py
+++ assignment-1/16307130383/source.py
@@ -1,15 +1,24 @@
 import os
 os.sys.path.append(os.path.abspath(os.path.dirname(__file__)) + '/..')
 # use the above line of code to surpass the top module barrier
-from handout import get_data
+from handout import get_data, gm1d
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.stats as ss
 
+
+#-------------------------
+# draw histogram estimation
+#-------------------------
 def draw_hist(data_num, bin_num):
   sampled_data = get_data(data_num)
   plt.hist(sampled_data, density=True, bins=bin_num)
   # plt.show()
 
+
+#-------------------------
+# draw kernel density estimation
+#-------------------------
 def count_gaussian(sampled_data, x, h):
   px = 0
   for sample in sampled_data:
@@ -40,6 +49,10 @@ def draw_kernel(data_num, h):
   plt.plot(x_array, y_array)
   # plt.show()
 
+
+#-------------------------
+# draw nearest neighbor estimation
+#-------------------------
 def get_volume(sampled_data, index, K):
   assert K <= len(sampled_data)
   # print('--------------')
@@ -70,45 +83,120 @@ def draw_nearest(data_num, K):
   plt.plot(x_array, y_array)
   # plt.show()
 
-fig = plt.figure(figsize=(15, 6))
-plt.subplot(4, 3, 1)
-plt.ylabel('10 bins')
-draw_hist(200, 10)
-plt.subplot(4, 3, 4)
-plt.ylabel('25 bins')
-draw_hist(200, 25)
-plt.subplot(4, 3, 7)
-plt.ylabel('50 bins')
-draw_hist(200, 50)
-plt.subplot(4, 3, 10)
-plt.ylabel('100 bins')
-draw_hist(200, 100)
-# plt.show()
 
-plt.subplot(4, 3, 2)
-plt.ylabel('h = 0.05')
-draw_kernel(100, 0.05)
-plt.subplot(4, 3, 5)
-plt.ylabel('h = 0.1')
-draw_kernel(100, 0.1)
-plt.subplot(4, 3, 8)
-plt.ylabel('h = 0.25')
-draw_kernel(100, 0.25)
-plt.subplot(4, 3, 11)
-plt.ylabel('h = 1.0')
-draw_kernel(100, 1.0)
-# plt.show()
+#-------------------------
+# explore answer for 4 questions
+#-------------------------
+def show_date_num_influence():
+  nums = [100, 500, 1000, 2000]
+  fig = plt.figure(figsize=(16, 6))
+  for i, num in enumerate(nums):
+    plt.subplot(4, 3, i*3+1)
+    plt.ylabel('num = {}'.format(num))
+    plt.xlabel('hist')
+    draw_hist(num, 50)
+    plt.subplot(4, 3, i*3+2)
+    plt.ylabel('num = {}'.format(num))
+    plt.xlabel('kernel')
+    draw_kernel(num, 0.2)
+    plt.subplot(4, 3, i*3+3)
+    plt.ylabel('num = {}'.format(num))
+    plt.xlabel('nearest')
+    draw_nearest(num, 15)
+  plt.show()
 
-plt.subplot(4, 3, 3)
-plt.ylabel('K = 2')
-draw_nearest(200, 2)
-plt.subplot(4, 3, 6)
-plt.ylabel('K = 5')
+def show_bin_method():
+  N = 200
+  sampled_data = get_data(N)
+  stdev = np.std(sampled_data)
+  # Sturge’s Rule   k = 1+log2(N)
+  # Scott’s Rule    h = 3.49σN^(−1/3)
+  # Rice’s Rule     k = pow(N, 1/3)*2
+  names = ['Sturge’s Rule', 'Scott’s Rule', 'Rice’s Rule', '', '', '', '']
+  bin_num = [int( 1 + np.ceil(np.log2(N)) ),
+            int(np.ceil( (max(sampled_data) - min(sampled_data)) / (3.49*stdev/np.power(N, 1.0/3.0)) )),
+            int(np.ceil( np.power(N, 1.0/3.0)*2 )), 20, 25, 30, 50]
+  print(bin_num)
+  fig = plt.figure(figsize=(6, 10))
+  for i, bins in enumerate(bin_num):
+    plt.subplot(3, 3, i+1)
+    plt.title(names[i])
+    plt.ylabel(bins)
+    draw_hist(N, bins)
+  plt.show()
+
+def show_h_influence():
+  N = 100
+  sampled_data = get_data(N)
+  sampled_data = sorted(sampled_data)
+  distance = 0
+  for i, sample in enumerate(sampled_data[1:]):
+    distance += sample - sampled_data[i-1]
+  distance /= (N-1)
+  print(distance)
+  # choose sqrt(avg(distance)) * 2
+  hs = [0.05, 0.1, 0.2, 0.3, 0.5, 1.0, 1.5, np.power(distance, 0.5)*2]
+  fig = plt.figure(figsize=(12, 6))
+  for i, h in enumerate(hs):
+    plt.subplot(4, 2, i+1)
+    plt.ylabel(h)
+    if i == 7:
+      plt.xlabel('sqrt(average_interval) * 2')
+    draw_kernel(N, h)
+  plt.show()
+
+def gm_plot(self, num_sample):
+    sampled_data = self.sample([num_sample])
+    min_range = min(self.modes) - 3 * self.std_range[1]
+    max_range = max(self.modes) + 3 * self.std_range[1]
+    xs = np.linspace(min_range, max_range, 2000)
+    ys = np.zeros_like(xs)
+    for l, s, w in zip(self.modes, self.stds, self.weights):
+        ys += ss.norm.pdf(xs, loc=l, scale=s) * w
+    plt.plot(xs, ys, scaley=2)
+
+def show_k_influence():
+  N = 200
+  sampled_data = get_data(N)
+  # Ks = [2, 5, 10, 20, 30, 40, 50, 60, 80, 100]
+  Ks = [2, 5, 20, 30]
+  fig = plt.figure(figsize=(12, 6))
+  for i, K in enumerate(Ks):
+    plt.subplot(4, 1, i+1)
+    plt.ylim(0, 0.35)
+    plt.ylabel('K = {}'.format(K))
+    draw_nearest(N, K)
+    gm_plot(gm1d, N)
+  plt.show()
+
+
+#-------------------------
+# draw final plots
+#-------------------------
+fig = plt.figure(figsize=(14, 6))
+plt.subplot(3, 2, 1)
+plt.ylabel('bin = {}'.format(20))
+plt.title('histogram estimation')
+draw_hist(200, 20)
+plt.subplot(3, 2, 5)
+plt.ylabel('h = {}'.format(0.5))
+plt.title('kernel density estimation')
+draw_kernel(100, 0.5)
+
+plt.subplot(3, 2, 2)
+plt.ylim(0, 0.35)
+plt.title('nearest neighbor estimation')
+plt.ylabel('K = {}'.format(5))
 draw_nearest(200, 5)
-plt.subplot(4, 3, 9)
-plt.ylabel('K = 10')
-draw_nearest(200, 10)
-plt.subplot(4, 3, 12)
-plt.ylabel('K = 20')
+gm_plot(gm1d, 200)
+plt.subplot(3, 2, 4)
+plt.ylim(0, 0.35)
+plt.ylabel('K = {}'.format(20))
 draw_nearest(200, 20)
+gm_plot(gm1d, 200)
+plt.subplot(3, 2, 6)
+plt.ylim(0, 0.35)
+plt.ylabel('K = {}'.format(30))
+draw_nearest(200, 30)
+gm_plot(gm1d, 200)
 plt.show()
