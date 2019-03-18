2ff3fd8e7dfb27266c48dfd445c626c2a7b1fc6d
yilin <yilinyan1030@gmail.com>
2019-03-18 14:52:07 +0800

    hist and kernel

--- assignment-1/16307130383/source.py
+++ assignment-1/16307130383/source.py
@@ -1,13 +1,35 @@
-# import os
-# os.sys.path.append('..')
-import sys,os
-sys.path.append(os.getcwd())
+import os
+os.sys.path.append(os.path.abspath(os.path.dirname(__file__)) + '/..')
 # use the above line of code to surpass the top module barrier
 from handout import get_data
 import numpy as np
 import matplotlib.pyplot as plt
 
+def draw_hist(data_num, bin_num):
+  sampled_data = get_data(data_num)
+  plt.hist(sampled_data, normed=True, bins=bin_num)
+  plt.show()
 
-sampled_data = get_data(500)
-plt.hist(sampled_data, normed=True, bins=50)
-plt.show()
+def count_gaussian(sampled_data, x, h):
+  px = 0
+  for sample in sampled_data:
+    px += np.exp( -np.power(x-sample, 2)/(2*h*h) )
+  px /= len(sampled_data) * np.power(2*np.pi*h*h, 0.5)
+  # print(px)
+  return px
+
+def draw_kernel(data_num, h):
+  sampled_data = get_data(data_num)
+  num_max = max(sampled_data)
+  num_min = min(sampled_data)
+  print(num_min)
+  print(num_max)
+  x_array = []
+  y_array = []
+  for i in range(0, int( (num_max-num_min)/h )):
+    x = i*h + h/2 + num_min
+    px = count_gaussian(sampled_data, x, h)
+    x_array.append(x)
+    y_array.append(px)
+  plt.plot(x_array, y_array)
+  plt.show()
