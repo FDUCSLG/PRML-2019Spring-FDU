25a34f0d535d9a5900f722da6f54d3112325ca06
Ruiyang-061X <17717073617@163.com>
2019-03-19 15:51:00 +0800

    submission of assignment-1 of 17307130064

--- assignment-1/17307130064/source.py
+++ assignment-1/17307130064/source.py
@@ -9,22 +9,22 @@ import math
 N = 200
 sampled_data = get_data(N)
 
-# bins_num = 50
-# plt.hist(sampled_data, normed=True, bins=bins_num)
-# plt.show()
+bins_num = 50
+plt.hist(sampled_data, normed=True, bins=bins_num)
+plt.show()
 
 
 min_range = min(sampled_data)
 max_range = max(sampled_data)
 x = np.linspace(min_range, max_range, 2000)
 px = np.empty(len(x))
-# h = 2
-#
-# for i in range(len(x)):
-#     px[i] = 1/N*np.sum( np.exp(-np.power((x[i]-np.array(sampled_data)), 2)/(2*h**2)) / pow((2*math.pi*h**2),0.5) )
-#
-# plt.plot(x, px)
-# plt.show()
+h = 2
+
+for i in range(len(x)):
+    px[i] = 1/N*np.sum( np.exp(-np.power((x[i]-np.array(sampled_data)), 2)/(2*h**2)) / pow((2*math.pi*h**2),0.5) )
+
+plt.plot(x, px)
+plt.show()
 
 
 K = 1
