8331e070dab50c708091caad9bb79eba83742236
EZ_hwh <240836592@qq.com>
2019-03-19 16:10:37 +0800

    submission of assignment-1 of 17307130155

---
+++ assignment-1/16307130177/source.py
@@ -0,0 +1,11 @@
+import os
+os.sys.path.append('..')
+# use the above line of code to surpass the top module barrier
+from handout import get_data
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+sampled_data = get_data(5000)
+plt.hist(sampled_data, normed=True, bins=10)
+plt.show()
