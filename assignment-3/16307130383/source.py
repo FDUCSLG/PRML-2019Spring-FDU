3990b884b5cc8082ad08b518ef70212a16b8fb99
yilin <yilinyan1030@gmail.com>
2019-05-16 10:44:55 +0800

    save and load model

--- assignment-3/16307130383/source.py
+++ assignment-3/16307130383/source.py
@@ -1,52 +1,94 @@
 import json
 import string
 import random
+import os
 import sys
 sys.path.append('../')
 import torch
 import torch.nn as nn
 import numpy as np
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+import pickle
 
-data = []
-with open('../poet.tang.20000.json', encoding='utf-8') as json_file:
-  data += json.load(json_file)
-with open('../poet.tang.21000.json', encoding='utf-8') as json_file:
-  data += json.load(json_file)
-with open('../poet.tang.22000.json', encoding='utf-8') as json_file:
-  data += json.load(json_file)
-with open('../poet.tang.23000.json', encoding='utf-8') as json_file:
-  data += json.load(json_file)
-with open('../poet.tang.24000.json', encoding='utf-8') as json_file:
-  data += json.load(json_file)
+def load_json():
+  # this finds our json files
+  path_to_json = '../json'
+  global json_files
+  json_files = [pos_json for pos_json in os.listdir(path_to_json) if pos_json.endswith('.json')]
+  data = []
+  # we need both the json and an index number so use enumerate()
+  for index, js in enumerate(json_files):
+      with open(os.path.join(path_to_json, js)) as json_file:
+          data += json.load(json_file)
+  paragraphs = [item['paragraphs'] for item in data]
+  random.Random(1).shuffle(paragraphs)
+  # write to disk
+  with open('paragraphs.list', 'wb') as fp:
+    pickle.dump(paragraphs, fp)
 
-paragraphs = [item['paragraphs'] for item in data]
-random.shuffle(paragraphs)
+paragraphs = []
+if os.path.exists('paragraphs.list'):
+  with open ('paragraphs.list', 'rb') as fp:
+    paragraphs = pickle.load(fp)
+else:
+  load_json()
 
 #####################
 # Preprocess
 #####################
 def get_vocab_seqs(sample_size=1000, seq_lenth=48):
-  assert sample_size <= 4000
-  # create vocab
-  words = ['EOS', 'OOV']
-  for paragraph in paragraphs[:sample_size]:
-    for sentence in paragraph:
-      for word in sentence:
-        words.append(word)
-  words = set(words)
-  vocab = {word: i for i, word in enumerate(words)}
-  # cut into fix length sentence
+  assert sample_size <= len(paragraphs)
+  words = []
+  count = {}
+  vocab = {}
   sequences = []
-  for paragraph in paragraphs[:sample_size]:
-    seq = ""
-    for sentence in paragraph:
-      seq += sentence
-      if len(seq) >= seq_lenth:
-        sequences.append(seq[:seq_lenth])
-        seq = ""
+  if os.path.exists('words.list'):
+    with open('words.list', 'rb') as fp:
+      words = pickle.load(fp)
+    with open('count.dict', 'rb') as fp:
+      count = pickle.load(fp)
+    with open('vocab.dict', 'rb') as fp:
+      vocab = pickle.load(fp)
+    with open('sequences.list', 'rb') as fp:
+      sequences = pickle.load(fp)
+  else:
+    # create vocab
+    words = ['EOS', 'OOV']
+    count = {'EOS': 100000, 'OOV': 100000}
+    for paragraph in paragraphs[:sample_size]:
+      for sentence in paragraph:
+        for word in sentence:
+          words.append(word)
+          if word in count:
+            count[word] += 1
+          else:
+            count[word] = 0
+    words = set([ x for x in words if count[x] > 1 ])  # at least occur 2 times
+    words = list(words)
+    vocab = {word: i for i, word in enumerate(words)}
+    # cut into fix length sentence
+    sequences = []
+    for paragraph in paragraphs[:sample_size]:
+      seq = ""
+      for sentence in paragraph:
+        seq += sentence
+        if len(seq) >= seq_lenth:
+          sequences.append(seq[:seq_lenth])
+          seq = ""
+    # write to disk
+    with open('words.list', 'wb') as fp:
+      pickle.dump(words, fp)
+    with open('count.dict', 'wb') as fp:
+      pickle.dump(count, fp)
+    with open('vocab.dict', 'wb') as fp:
+      pickle.dump(vocab, fp)
+    with open('sequences.list', 'wb') as fp:
+      pickle.dump(sequences, fp)
   # seq to int
   xs, ys = seq_to_int(vocab, sequences)
-  return list(words), vocab, sequences, xs, ys  # translate words into list
+  return count, words, vocab, sequences, xs, ys  # translate words into list
 
 def seq_to_int(vocab, sequences):
   xs = []
@@ -105,58 +147,57 @@ class LSTM(nn.Module):
 # Init
 #####################
 seq_len = 48  # 句长
-sample_size = 2048  # 诗个数
-words, vocab, sequences, X_int, y_int = get_vocab_seqs(sample_size, seq_len)
+sample_size = 8000  # 诗个数
+word_count, words, vocab, sequences, X_int, y_int = get_vocab_seqs(sample_size, seq_len)
 vocab_size = len(words)
-input_dim = 256  # 单个字向量长
-hidden_dim = 256
+input_dim = 256  # 输入向量长
+hidden_dim = 512
 batch_size = 32
 output_dim = vocab_size
-num_layers = 2
+num_layers = 1
 model = LSTM(vocab_size=vocab_size, input_dim=input_dim, hidden_dim=hidden_dim, 
         batch_size=batch_size, output_dim=output_dim, num_layers=num_layers)
-# # X_train and y_train & target
-# X_transpose = np.array(X_int[:batch_size]).transpose()
-# X_onehot = np.eye(input_dim)[X_transpose]
-# X_train = torch.from_numpy(X_onehot).type(torch.Tensor)
-# y_transpose = np.array(y_int[:batch_size]).transpose()
-# y_train = torch.from_numpy(y_transpose).type(torch.LongTensor)
-# target = y_train.contiguous().view(-1)
-# loss_fn and optimiser
+# loss_fn & optimiser 
 loss_fn = torch.nn.CrossEntropyLoss(reduction='sum')
 learning_rate = 0.01
 optimiser = torch.optim.Adam(model.parameters(), lr=learning_rate)
 # claim globally
-X_train = []
-y_train = []
+X_train_array = []
+target_array = []
 hist = []
-batch_start = 0
+# preprocess X_train, y_train
+for batch_start in range(0, len(X_int) - batch_size, batch_size):
+  l = batch_start
+  r = batch_start + batch_size
+  X_transpose = np.array(X_int[l:r]).transpose()
+  X_train = torch.from_numpy(X_transpose).type(torch.LongTensor)
+  y_transpose = np.array(y_int[l:r]).transpose()
+  y_train = torch.from_numpy(y_transpose).type(torch.LongTensor)
+  target = y_train.contiguous().view(-1)
+  X_train_array.append(X_train)
+  target_array.append(target)
 
 def get_var():
-  return {'X': X_train, 'y': y_train, 'V': vocab, 'seq': sequences, 'm': model, 'w': words, 'h': hist}
+  return {'X': X_train_array, 'y': target_array, 'V': vocab, 'seq': sequences, 'm': model, 'w': words, 'h': hist, 'c': word_count}
 
 #####################
 # Trainer
 #####################
-def train(num_epochs):
+def train(num_epochs, batch_end=len(X_train_array)):
   global hist
   hist = np.zeros(num_epochs)
   for t in range(num_epochs):
-    global batch_start
-    # batch_start = (batch_start + batch_size) % len(X_int)
-    # 保证 l r 不会横跨末尾
-    if batch_start < batch_size:
-      batch_start += batch_size
-    l = batch_start - batch_size
-    r = batch_start
-    # X_train and y_train & target
-    global X_train
-    global y_train
-    X_transpose = np.array(X_int[l:r]).transpose()
-    X_train = torch.from_numpy(X_transpose).type(torch.LongTensor)
-    y_transpose = np.array(y_int[l:r]).transpose()
-    y_train = torch.from_numpy(y_transpose).type(torch.LongTensor)
-    target = y_train.contiguous().view(-1)
+    # # adjust learning rate
+    # if t == 100:
+    #   global optimiser
+    #   for g in optimiser.param_groups:
+    #     g['lr'] = 0.002
+    # get batch
+    global X_train_array
+    global target_array
+    batch_num = t % batch_end
+    X_train = X_train_array[batch_num]
+    target = target_array[batch_num]
     # Clear stored gradient
     model.zero_grad()
     # Initialise hidden state
@@ -168,12 +209,15 @@ def train(num_epochs):
     output = y_pred.view(-1, vocab_size)
     loss = loss_fn(output, target)
     hist[t] = loss.item()
-    if t % 32 == 0:
-      print("from ", l, " to ", r)
+    if t % 16 == 0:
+      print("batch num: ", batch_num)
       print("Epoch ", t, "CE: ", loss.item())
-      print("sample input: ", translate_int(X_int[l]))
-      print("sample output: ", translate_vec(y_pred.detach().numpy().transpose().tolist()[0]))
-      print(y_pred[0])
+      print("sample input: ", translate_int( X_train.numpy().transpose().tolist()[0] ))
+      print("sample output: ", translate_vec( y_pred.detach().permute(1, 0, 2).numpy().tolist()[0] ))
+      # print("sample input: ", translate_int( X_train.numpy().transpose().tolist()[1] ))
+      # print("sample output: ", translate_vec( y_pred.detach().permute(1, 0, 2).numpy().tolist()[1] ))
+      # print(y_pred.detach().permute(1, 0, 2).shape)
+      # print(X_train.shape)
     # Zero out gradient, else they will accumulate between epochs
     optimiser.zero_grad()
     # backward
@@ -181,25 +225,16 @@ def train(num_epochs):
     # Update parameters
     optimiser.step()
 
-def translate_int(sentence):  # only accept list
-  seq = ""
-  for idx in sentence:
-    seq += words[idx]
-  return seq
-
-def translate_vec(sentence):  # only accept list
-  seq = ""
-  for vec in sentence:
-    idx = np.argmax(vec)
-    seq += words[idx]
-  return seq
-
+#####################
+# Generate
+#####################
 def generate(seed=None, length=48):
   # set up first char
   start = [[ random.randint(0, vocab_size-1) ]]
   if not seed is None:
     if seed in words:
       start = [[ vocab[seed] ]]
+      print( vocab[seed] )
     else:
       start = [[ vocab['OOV'] ]]
   start = torch.LongTensor(start)
@@ -215,14 +250,39 @@ def generate(seed=None, length=48):
     y_pred = model.linear(lstm_out.view(1, 1, -1))
     y_pred = nn.functional.softmax(y_pred, dim=2)
     # get next
-    print(y_pred[0])
-    next_char = translate_vec(y_pred.detach().numpy().transpose().tolist()[0])
+    # print(y_pred[0])
+    next_char = translate_vec( y_pred.detach().permute(1, 0, 2).numpy().tolist()[0] )
     seq += next_char
     start = torch.LongTensor([[ vocab[next_char] ]])
   print(seq)
 
-# >>> target = torch.empty(1, dtype=torch.long).random_(10)
-# >>> target
-# tensor([9])
-# >>> loss_fn(y_pred[0], target)
-# tensor(2.3179, grad_fn=<NllLossBackward>)
+#####################
+# Utilities
+#####################
+def translate_int(sentence):  # only accept list
+  # print('tranlate: ', sentence)
+  seq = ""
+  for idx in sentence:
+    seq += words[idx]
+  return seq
+
+def translate_vec(sentence):  # only accept list
+  seq = ""
+  for vec in sentence:
+    idx = np.argmax(vec)
+    seq += words[idx]
+  return seq
+
+def save(path):
+  torch.save(model.state_dict(), path)
+
+def load(path):
+  model.load_state_dict(torch.load(path))
+  model.eval()
+
+def draw():
+  X = [i for i in hist if i != 0]
+  line_x = np.linspace(0, len(X), len(X))
+  line_y = np.array(X)
+  plt.plot(line_x, line_y)
+  plt.show()
