996263b17a1e26f11f03c61dc7386aa3d8d07c3d
yilin <yilinyan1030@gmail.com>
2019-05-21 22:29:03 +0800

    CNN & trainner

--- assignment-4/16307130383/model.py
+++ assignment-4/16307130383/model.py
@@ -2,12 +2,9 @@ import torch
 import torch.nn as nn
 from torch.nn import Parameter
 import numpy as np
-import matplotlib
-matplotlib.use('TkAgg')
-import matplotlib.pyplot as plt
 
 class RNNcell(nn.Module):
-  def __init__(input_dim, hidden_dim):
+  def __init__(self, input_dim, hidden_dim):
     super().__init__()
     self.input_dim, self.hidden_dim = input_dim, hidden_dim
     self.W_ih = Parameter(torch.Tensor(input_dim, hidden_dim))
@@ -34,7 +31,7 @@ class RNNcell(nn.Module):
     return hidden_seq[-1], h_t
 
 class RNN(nn.Module):
-  def __init__(vocab_size, input_dim, hidden_dim, output_dim):
+  def __init__(self, vocab_size, input_dim, hidden_dim, output_dim):
     super().__init__()
     self.vocab_size = vocab_size
     self.input_dim = input_dim
@@ -43,16 +40,59 @@ class RNN(nn.Module):
 
     self.embed = nn.Embedding(vocab_size, input_dim)
     self.rnn = RNNcell(input_dim, hidden_dim)
-    self.linear = nn.linear(hidden_dim, output_dim)
+    self.linear = nn.Linear(hidden_dim, output_dim)
 
   def init_hidden(self, batch_size=32):
     self.hidden = torch.zeros(batch_size, self.hidden_dim)
 
-  def forward(self, input):
-    # input: [batch_size, seq_len] -> [batch_size, seq_len, input_dim] -> [seq_len, batch_size, input_dim]
-    input = self.embed(input).permute(1, 0, 2)
+  def forward(self, word_seq):
+    # init
+    self.init_hidden(batch_size=len(word_seq))
+    # word_seq: [batch_size, seq_len] -> [batch_size, seq_len, word_seq_dim] -> [seq_len, batch_size, word_seq_dim]
+    word_seq = self.embed(word_seq).permute(1, 0, 2)
     # forward
-    rnn_out, self.hidden = self.rnn(input, self.hidden)
+    rnn_out, self.hidden = self.rnn(word_seq, self.hidden)
     y_pred = self.linear(rnn_out)
     # output: [batch_size, output_dim]
-    return { 'output': y_pred }
+    return { 'output': y_pred, 'pred': nn.functional.softmax(y_pred, dim=1), 'hidden': self.hidden }
+
+
+class CNN(nn.Module):
+  def __init__(self, vocab_size, input_dim, output_dim, in_channels, out_channels,
+                kernel_sizes, keep_probab, embedding_weights=None):
+    super().__init__()
+    self.vocab_size = vocab_size
+    self.input_dim = input_dim
+    self.output_dim = output_dim
+    self.in_channels = in_channels
+    self.out_channels = out_channels
+    self.kernel_sizes = kernel_sizes
+
+    self.embed = nn.Embedding(vocab_size, input_dim)
+    # self.embed.weight = nn.Parameter(embedding_weights, requires_grad=False)
+    self.conv1 = nn.Conv2d(in_channels, out_channels, (kernel_sizes[0], input_dim))
+    self.conv2 = nn.Conv2d(in_channels, out_channels, (kernel_sizes[1], input_dim))
+    self.conv3 = nn.Conv2d(in_channels, out_channels, (kernel_sizes[2], input_dim))
+    self.dropout = nn.Dropout(keep_probab)
+    self.linear = nn.Linear(len(kernel_sizes)*out_channels, output_dim)
+
+  def conv_calc(self, input, conv_fn):
+    conv_out = conv_fn(input)  # conv_out: [batch_size, out_channels, dim, 1]
+    conv_relu = nn.functional.relu(conv_out.squeeze(3))  # conv_relu: [batch_size, out_channels, dim1]
+    max_out = nn.functional.max_pool1d(conv_relu, conv_relu.size(2)).squeeze(2)  # max_out: [batch_size, out_channels]
+    return max_out
+
+  def forward(self, word_seq, batch_size=None):
+    # input: [batch_size, num_seq] -> [batch_size, 1, num_seq, embedding_length]
+    input = self.embed(word_seq)
+    input = input.unsqueeze(1)
+    # max_out: [batch_size, out_channels]
+    max_out1 = self.conv_calc(input, self.conv1)
+    max_out2 = self.conv_calc(input, self.conv2)
+    max_out3 = self.conv_calc(input, self.conv3)
+    # all_out: [batch_size, num_kernels*out_channels]
+    all_maxout = torch.cat((max_out1, max_out2, max_out3), 1)
+    all_dropout = self.dropout(all_maxout)
+    # logits: [batch_size, output_dim]
+    logits = self.linear(all_dropout)
+    return { 'output': logits, 'pred': nn.functional.softmax(logits, dim=1) }
--- assignment-4/16307130383/preprocess.py
+++ assignment-4/16307130383/preprocess.py
@@ -1,3 +1,4 @@
+import os
 import sys
 sys.path.append('..')
 import string
@@ -6,20 +7,7 @@ import re
 from sklearn.datasets import fetch_20newsgroups, load_files
 from fastNLP import Vocabulary, DataSet, Instance
 
-def get_20newsgroups_data():
-  categories = ['comp.graphics',
-                'comp.os.ms-windows.misc',
-                'comp.sys.mac.hardware',
-                'misc.forsale',
-                'rec.motorcycles',
-                'rec.sport.baseball',
-                'sci.crypt',
-                'sci.electronics',
-                'sci.space',
-                'soc.religion.christian',
-                'talk.politics.guns',
-                'talk.politics.mideast',
-                'talk.religion.misc']
+def get_20newsgroups_data(categories):
   dataset_train = fetch_20newsgroups(subset='train', shuffle=True, categories=categories, data_home='../data/') # , download_if_missing=False)
   dataset_test = fetch_20newsgroups(subset='test', shuffle=True, categories=categories, data_home='../data/') # , download_if_missing=False)
   print("In training dataset:")
@@ -42,8 +30,14 @@ def create_dataset(data, sample_size):
   data_set.apply(lambda x: sentence_to_words( x['raw_sentence'] ), new_field_name='word_seq')
   return data_set
 
-def build_dataset(train_size, test_rate):
-  train, test = get_20newsgroups_data()
+def build_dataset(train_size, test_rate, categories):
+  vocab = load('../data/vocab')
+  train_set = load('../data/train_set')
+  test_set = load('../data/test_set')
+  if not vocab is None and not train_set is None and not test_set is None:
+    return vocab, train_set, test_set
+  
+  train, test = get_20newsgroups_data(categories)
   train_set = create_dataset(train, train_size)
   test_set = create_dataset(test, int(train_size * test_rate))
   # vocabulary
@@ -51,9 +45,26 @@ def build_dataset(train_size, test_rate):
   test_set.apply(lambda x: [vocab.add(word) for word in x['word_seq']])
   vocab.build_vocab()
   # word_seq to int
-  train_set.apply(lambda x: [vocab.to_index(word) for word in x['word_seq']], new_field_name='input')
-  test_set.apply(lambda x: [vocab.to_index(word) for word in x['word_seq']], new_field_name='input')
+  train_set.apply(lambda x: [vocab.to_index(word) for word in x['word_seq']], new_field_name='word_seq')
+  test_set.apply(lambda x: [vocab.to_index(word) for word in x['word_seq']], new_field_name='word_seq')
   # tag
-  train_set.set_input('input')
+  train_set.set_input('word_seq')
   train_set.set_target('target')
+  test_set.set_input('word_seq')
+  test_set.set_target('target')
+
+  save('../data/vocab', vocab)
+  save('../data/train_set', train_set)
+  save('../data/test_set', test_set)
   return vocab, train_set, test_set
+
+def load(filename):
+  if os.path.exists(filename):
+    with open (filename, 'rb') as fp:
+      return pickle.load(fp)
+  else:
+    return None
+
+def save(filename, item):
+  with open(filename, 'wb') as fp:
+    pickle.dump(item, fp)
