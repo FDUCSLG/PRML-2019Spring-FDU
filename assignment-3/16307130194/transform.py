e65944e050aafc7e246e45538728d0f688db8062
Zhongyu Chen <zhongyuchen@yahoo.com>
2019-05-22 21:44:46 +0800

    transform

--- assignment-3/16307130194/transform.py
+++ assignment-3/16307130194/transform.py
@@ -4,6 +4,9 @@ from langconv import *
 
 data = np.load('tang.npz')
 data, ix2word = data['data'], data['ix2word'].item()
+print(data[0])
+print(ix2word[data[0][-3]])
+
 poetry = []
 for sentence in data:
     text = ''
@@ -11,10 +14,12 @@ for sentence in data:
         text += ix2word[d]
     poetry.append(text)
 
+print(poetry[0])
 
-with open('tangshi.txt', 'w') as f:
-    for i, line in poetry:
-        line = Converter('zh-hans').convert(line.decode('utf-8')).encode('utf-8')
+with open('tangshi.txt', 'w', encoding='utf-8') as f:
+    length = len(poetry)
+    for i, line in enumerate(poetry):
+        line = Converter('zh-hans').convert(line)
         if i < length - 1:
             f.write(line + '\n\n')
         else:
