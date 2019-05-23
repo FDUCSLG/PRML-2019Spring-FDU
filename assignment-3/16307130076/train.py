5d588158ec50ca16a09d9f4faea7978a7de86407
ZhaoWeicheng98 <zhaoweicheng98@gmail.com>
2019-05-23 17:55:50 +0800

    Fix train

--- assignment-3/16307130076/train.py
+++ assignment-3/16307130076/train.py
@@ -16,13 +16,17 @@ test_data, train_data, idx2word, word2idx = construct_vec(
     test_data, train_data)
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--batch_size", "-bs", type=int,default=16)
-parser.add_argument("--seq_len", "-sl", type=int,default=40)
-parser.add_argument("--seq_step", "-ss", type=int,default=10)
-parser.add_argument("--hidden_size", "-hs", type=int,default=200)
-parser.add_argument("--input_size", "-is", type=int,default=200)
-parser.add_argument("--max_epoch", "-me", type=int,default=20)
-parser.add_argument("--learning_rate", "-lr", type=float,default=1e-3)
+parser.add_argument("--batch_size", "-bs", type=int, default=16)
+parser.add_argument("--seq_len", "-sl", type=int, default=40)
+parser.add_argument("--seq_step", "-ss", type=int, default=10)
+parser.add_argument("--hidden_size", "-hs", type=int, default=200)
+parser.add_argument("--input_size", "-is", type=int, default=200)
+parser.add_argument("--max_epoch", "-me", type=int, default=20)
+parser.add_argument("--learning_rate", "-lr", type=float, default=1e-3)
+parser.add_argument("--optimizer", "-o", type=str,
+                    default="Adam", choices=["Adam", "SGD"])
+parser.add_argument("--momentum", "-m", type=float, default=0.9)
+parser.add_argument("--tolerance", "-t", type=int, default=2)
 arg = parser.parse_args()
 batch_size = arg.batch_size
 seq_length = arg.seq_len
@@ -33,9 +37,10 @@ max_epoch = arg.max_epoch
 lr = arg.learning_rate
 train_data = PoemSet(train_data, seq_length, seq_step, idx2word, word2idx)
 test_data = PoemSet(test_data, seq_length, seq_step, idx2word, word2idx)
-print(len(train_data),len(test_data))
-dataloader = DataLoader(train_data, batch_size,shuffle=True)
+print(len(train_data), len(test_data))
+dataloader = DataLoader(train_data, batch_size, shuffle=True)
 test_dataloader = DataLoader(test_data, batch_size, shuffle=True)
+
 
 def train():
     net = TangPoemGenerator(input_size, hidden_size, len(idx2word))
@@ -43,8 +48,11 @@ def train():
     use_cuda = torch.cuda.is_available()
     if use_cuda:
         net.cuda()
-    optimizer = Adam(net.parameters(recurse=True), lr=lr)
-    last_p = float("inf")
+    if arg.optimizer == "Adam":
+        optimizer = Adam(net.parameters(recurse=True), lr=lr)
+    elif arg.optimizer == "SGD":
+        optimizer = SGD(net.parameters(recurse=True), lr=lr,momentum=arg.momentum)
+    p = []
     for epoch in range(max_epoch):
         net.train()
         losses = 0
@@ -52,27 +60,27 @@ def train():
             t0 = time.time()
             batch_data = batch_data.transpose(1, 0).contiguous()
             if use_cuda:
-                batch_data =batch_data.cuda()
-            input, target = batch_data[:-1,:], batch_data[1:,:]
+                batch_data = batch_data.cuda()
+            input, target = batch_data[:-1, :], batch_data[1:, :]
             output = net(input)
             optimizer.zero_grad()
-            loss = criterion(output,target.view(-1))
+            loss = criterion(output, target.view(-1))
             loss.backward()
             optimizer.step()
             losses += loss.item()
             t1 = time.time()
-            if idx%10==0:
+            if idx % 10 == 0:
                 print("epoch:", epoch, " step:", idx,
                       " loss:", losses/(idx+1), " time:", t1-t0)
         torch.save(net.state_dict(), '%s_%s.pth' %
                    ("Tang", epoch))
         perplexity = eval(net)
         print("perplexity:", perplexity)
-        if perplexity > last_p:
+        p.append(perplexity)
+        if epoch >= arg.tolerance and perplexity > p[epoch-arg.tolerance]:
             print("early stop")
             break
-        else:
-            last_p = perplexity
+
 
 def eval(net):
     net.eval()
@@ -87,10 +95,11 @@ def eval(net):
             input, target = data[:-1, :], data[1:, :]
             output = net(input)
             loss = criterion(output, target.view(-1))
-            p=torch.exp(loss).item()
+            p = torch.exp(loss).item()
             perplexity += p/len(test_dataloader)
-            print("testing %d/%d,perplexity:%f" % (idx, len(test_dataloader),p))
+            print("testing %d/%d,perplexity:%f" %
+                  (idx, len(test_dataloader), p))
     return perplexity
 
-train()
 
+train()
