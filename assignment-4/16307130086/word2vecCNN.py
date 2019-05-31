8471d191e0214b291fdd34674e39a4ef8aa83ca6
hhcyforever <changyang.he.young@gmail.com>
2019-05-31 11:02:13 +0800

    add word2vec

--- assignment-4/16307130086/word2vecCNN.py
+++ assignment-4/16307130086/word2vecCNN.py
@@ -33,23 +33,26 @@ trainData = {
 }
 trainData = DataSet(trainData)
 testData = DataSet(testData)
-allData = testSet['data']+trainSet['data']
-for i in range(len(allData)):
-    allData[i] = allData[i].lower().split()
+
 #     allData[i].append("<pad>")
 
-
-W2Vmodel = gensim.models.Word2Vec(min_count=2)
 trainData.apply(lambda x: x['data'].lower(), new_field_name='sentence')
 trainData.apply(lambda x: x['sentence'].split(), new_field_name='words', is_input=True)
 vocab = Vocabulary(min_freq=2)
 vocab = vocab.from_dataset(trainData, field_name='words')
+allData = trainData['words']
+for i in range(len(allData)):
+    allData[i].append("<pad>")
+    allData[i].append("<unk>")
+W2Vmodel = gensim.models.Word2Vec(allData, min_count=2, iter=15, size=300)
+W2Vmodel.wv.save_word2vec_format("w2v.model")
+model = gensim.models.KeyedVectors.load_word2vec_format('w2v.model')
+weights = torch.FloatTensor(model.vectors)
 #change to index
 
 vocab.index_dataset(trainData, field_name='words',new_field_name='words')
 trainData.set_target('target')
 train_data, dev_data = trainData.split(0.2)
-
 
 from torch.nn import Parameter
 class w2vCNNText(torch.nn.Module):
@@ -57,20 +60,19 @@ class w2vCNNText(torch.nn.Module):
     def __init__(self, init_embed,
                  num_classes,
                  vocab,
-                 W2Vmodel,
-                 kernel_nums=(2, 3, 4, 5),
+                 pretrained_weight,
+                 kernel_nums=(10,10,10,10),
                  kernel_sizes=(2, 3, 4, 5),
                  padding=0,
                  dropout=0.5):
         super(w2vCNNText, self).__init__()
         
         # no support for pre-trained embedding currently
-        self.embedding_dim = 100
-        
-        self.vocab = vocab
-        self.W2Vmodel = W2Vmodel
+        self.word_embeds = nn.Embedding(init_embed[0],init_embed[1])
+        pretrained_weight = np.array(pretrained_weight)
+        self.word_embeds.weight.data.copy_(torch.from_numpy(pretrained_weight))
         self.conv_pool = encoder.ConvMaxpool(
-            in_channels=self.embedding_dim,
+            in_channels=init_embed[1],
             out_channels=kernel_nums,
             kernel_sizes=kernel_sizes,
             padding=padding)
@@ -80,17 +82,18 @@ class w2vCNNText(torch.nn.Module):
     def forward(self, words, seq_len=None):
         batch_size = words.size()[0]
         seq_len = words.size()[1]
-        w2vWords = torch.DoubleTensor(batch_size, seq_len, self.embedding_dim)
-        for i in range(batch_size):
-            for j in range(seq_len):
-                word = self.vocab.to_word(words[i][j].item())
-                if word in self.W2Vmodel:
-                    w2vWords[i][j] = torch.tensor(self.W2Vmodel[word])
-                else:
-#                     print(word)
-                    w2vWords[i][j] = torch.zeros(self.embedding_dim)
-        w2vWords = w2vWords.float()
-        x = self.conv_pool(w2vWords)  
+#         w2vWords = torch.DoubleTensor(batch_size, seq_len, self.embedding_dim)
+#         for i in range(batch_size):
+#             for j in range(seq_len):
+#                 word = self.vocab.to_word(words[i][j].item())
+#                 if word in self.W2Vmodel:
+#                     w2vWords[i][j] = torch.tensor(self.W2Vmodel[word])
+#                 else:
+# #                     print(word)
+#                     w2vWords[i][j] = torch.zeros(self.embedding_dim)
+#         w2vWords = w2vWords.float()
+        x = self.word_embeds(words)
+        x = self.conv_pool(x)  
         x = self.dropout(x)
         x = self.fc(x)  
         return {C.OUTPUT: x}
@@ -107,7 +110,7 @@ vocab = vocab.from_dataset(trainData, field_name='words')
 #change to index
 vocab.index_dataset(trainData, field_name='words',new_field_name='words')
 trainData.set_target('target')
-model = w2vCNNText((len(vocab),128), num_classes=20, vocab=vocab, W2Vmodel=W2Vmodel, padding=2, dropout=0.1)
+model = w2vCNNText((len(vocab),300), num_classes=20, vocab=vocab, pretrained_weight=weights, padding=2, dropout=0.1)
 train_data, dev_data = trainData.split(0.2)
 trainer = Trainer(model=model, train_data=train_data, dev_data=dev_data, loss=CrossEntropyLoss(), metrics=AccuracyMetric(), batch_size=16)
 trainer.train()
